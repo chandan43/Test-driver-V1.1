6ff8c4ad258c74fcf41470806d5bc53937e73c59
Chandan <jha2011eec43@gmail.com>
2017-10-17 19:03:31 +0530

    added clock

---
+++ char/clock.h
@@ -0,0 +1,15 @@
+
+
+#define RTC_ADDRESS_BASE 0x70
+#define RTC_DATA_BASE 0x71
+
+#define SECOUND_CMD 0
+#define MINUTES_CMD 2
+#define HOURS_CMD 4
+#define DAY_CMD 7
+#define MONTH_CMD 8
+#define YEAR_CMD 9
+
+#define 
+
+
