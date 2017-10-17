290b2bd476b4d000ddcc27aca5875d1626bd9257
Chandan <jha2011eec43@gmail.com>
2017-10-17 16:03:01 +0530

    Added assignment question

---
+++ Assignment/Test_read.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#define err_handelr(en,msg) do{errno=en; perror(msg);exit(EXIT_SUCCESS);}while(0)
+#define DEVICE_NAME "/dev/char_dev"
+#define SIZE 1024
+
+static char message[SIZE];
+int main(){
+	int fd,ret;
+	fd= open(DEVICE_NAME,O_RDWR);
+	if(fd<0)
+		err_handelr(fd,"open");
+	printf("Press Enter to read from device..\n");
+	getchar();
+	printf("Reading from Device...\n");
+	ret=read(fd,message,SIZE);
+	if(ret<0)
+		err_handelr(ret,"read");
+	printf("The received message is: [%s]\n",message);
+	return 0;
+}
+
