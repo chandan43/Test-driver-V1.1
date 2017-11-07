c03a5c2635bf086d80031c8547b258f9710ba413
Chandan <jha2011eec43@gmail.com>
2017-11-07 18:44:23 +0530

    Added config register and updated probe function :-rtl8139

--- net/realtek/rtl8139.c
+++ net/realtek/rtl8139.c
@@ -63,7 +63,8 @@ They are defined in include/linux/compiler.h:
 #endif
 
 /* A few user-configurable values. */
-/* media options */
+/* media options  
+ * (-) indicates, media and duplex will reset when link-status goes down.*/
 #define MAX_UNITS 8
 static int media[MAX_UNITS] = {-1, -1, -1, -1, -1, -1, -1, -1};
 static int full_duplex[MAX_UNITS] = {-1, -1, -1, -1, -1, -1, -1, -1};
@@ -337,18 +338,338 @@ enum Config1Bits {
 	Cfg1_PIO	= 0x04,
 	Cfg1_MMIO	= 0x08,
 	LWAKE		= 0x10,		/* not on 8139, 8139A */
-	Cfg1_Driver_Load = 0x20,
+	Cfg1_Driver_Load= 0x20,
 	Cfg1_LED0	= 0x40,
 	Cfg1_LED1	= 0x80,
-	SLEEP		= (1 << 1),	/* only on 8139, 8139A : Set to enable Vital Product Data: The VPD data is stored in 93C46 from within offset 40h-7Fh.*/
-	PWRDN		= (1 << 0),	/* only on 8139, 8139A : 1  means A(bit 4 of the Status Register) in the PCI)=1, B(Cap_Ptr register)=50h, C(power management)=01h, D(PM registers) valid, E=0*/
+	SLEEP		= (1 << 1),/* only on 8139, 8139A : Set to enable Vital Product Data: The VPD data is stored in 93C46 from within offset 40h-7Fh.*/
+	PWRDN		= (1 << 0),/* only on 8139, 8139A : 1  means A(bit 4 of the Status Register) in the PCI)=1, B(Cap_Ptr register)=50h, C(power management)=01h, D(PM registers) valid, E=0*/
 };
 
+/* Bits in Config3 */
+enum Config3Bits {
+	Cfg3_FBtBEn   	= (1 << 0), /* 1	= Fast Back to Back */
+	Cfg3_FuncRegEn	= (1 << 1), /* 1	= enable CardBus Function registers */
+	Cfg3_CLKRUN_En	= (1 << 2), /* 1	= enable CLKRUN */
+	Cfg3_CardB_En 	= (1 << 3), /* 1	= enable CardBus registers */
+	Cfg3_LinkUp   	= (1 << 4), /* 1	= wake up on link up */
+	Cfg3_Magic    	= (1 << 5), /* 1	= wake up on Magic Packet (tm) */
+	Cfg3_PARM_En  	= (1 << 6), /* 0	= software can set twister parameters */
+	Cfg3_GNTSel   	= (1 << 7), /* 1	= delay 1 clock from PCI GNT signal */
+};
 
-/* New device inserted */
-static int  rtl8139_init_one(struct pci_dev *dev, const struct pci_device_id *ent)
+/* Bits in Config4 */
+enum Config4Bits {
+	LWPTN	= (1 << 2),	/* not on 8139, 8139A : LWAKE pattern: Please refer to LWACT bit : 4 in CONFIG1 register. */
+};
+
+/* Bits in Config5 */
+enum Config5Bits {
+	Cfg5_PME_STS   	= (1 << 0), /* 1	= PCI reset resets PME_Status */
+	Cfg5_LANWake   	= (1 << 1), /* 1	= enable LANWake signal */
+	Cfg5_LDPS      	= (1 << 2), /* 0	= save power when link is down */
+	Cfg5_FIFOAddrPtr= (1 << 3), /* Realtek internal SRAM testing : 1: Both Rx and Tx FIFO address pointers are updated in descending , */
+	Cfg5_UWF        = (1 << 4), /* 1 = accept unicast wakeup frame */
+	Cfg5_MWF        = (1 << 5), /* 1 = accept multicast wakeup frame */
+	Cfg5_BWF        = (1 << 6), /* 1 = accept broadcast wakeup frame */
+};
+/*
+1.Rx FIFO Threshold: 15-13 R/W RXFTH2, 1, 0   Specifies Rx FIFO Threshold level ,Whenever Rx FIFO, has reached to this level the receive PCI bus master function
+  will begin to transfer the data from the FIFO to the host memory . 
+  111 = no rx threshold. begins the transfer of data after having received a whole packet in the FIFO
+2.Max DMA Burst Size per Rx DMA Burst: 10-8 R/W MXDMA2, 1, 0, This field sets the maximum size of the receive DMA data bursts according to the following table:
+  111 = unlimited
+3.Rx Buffer Length: 12-11 R/W RBLEN1, 0  ,This field indicates the size of the Rx ring buffer. 00 = 8k + 16 byte 01 = 16k + 16 byte 10 = 32K + 16 byte 11 = 64K + 16 byte
+*/
+enum RxConfigBits {
+	/* rx fifo threshold */
+	RxCfgFIFOShift	= 13,
+	RxCfgFIFONone	= (7 << RxCfgFIFOShift),
+
+	/* Max DMA burst */
+	RxCfgDMAShift	= 8,
+	RxCfgDMAUnlimited = (7 << RxCfgDMAShift),
+
+	/* rx ring buffer length */
+	RxCfgRcv8K	= 0,
+	RxCfgRcv16K	= (1 << 11),
+	RxCfgRcv32K	= (1 << 12),
+	RxCfgRcv64K	= (1 << 11) | (1 << 12),
+
+	/* Disable packet wrap at end of Rx buffer. (not possible with 64k) */
+	RxNoWrap	= (1 << 7),
+};
+/* Twister tuning parameters from RealTek.
+   Completely undocumented, but required to tune bad links on some boards. */
+/*
+15        Testfun                1 = Auto-neg speeds up internal timer
+14-10       -                    Reserved
+ 9          LD                   Active low TPI link disable signal. When low, TPI still transmits
+                                         link pulses and TPI stays in good link state.
+ 8        HEART BEAT             1 = HEART BEAT enable, 0 = HEART BEAT disable. HEART
+ 7            JBEN               BEAT function is only valid in 10Mbps mode.
+                                        1 = enable jabber function; 0 = disable jabber function
+ 6        F_LINK_100              Used to login force good link in 100Mbps for diagnostic purposes.
+                                         1 = DISABLE, 0 = ENABLE.
+ 5         F_Connect              Assertion of this bit forces the disconnect function to be bypassed.
+ 4           -                    Reserved
+ 3        Con_status              This bit indicates the status of the connection. 1 = valid connected
+                                  link detected; 0 = disconnected link detected.
+ 2        Con_status_En           Assertion of this bit configures LED1 pin to indicate connection
+                                  status.
+ 1             -                  Reserved
+ 0          PASS_SCR              Bypass Scramble
+ */
+enum CSCRBits {
+	CSCR_LinkOKBit		= 0x0400,
+	CSCR_LinkChangeBit	= 0x0800,
+	CSCR_LinkStatusBits	= 0x0f000,
+	CSCR_LinkDownOffCmd	= 0x003c0,
+	CSCR_LinkDownCmd	= 0x0f3c0,
+}
+/*
+  9346CR: 93C46 Command Register : Bit 7-6 , R/W EEM1-0 ,Operating Mode: These 2 bits select the RTL8139D(L) operating mode, 
+  00 : Normal (RTL8139D(L) network/host communication
+  01 : Auto-load: Entering this mode
+  10 : 93C46 programming
+  11 : Config register write enable: Before writing to CONFIG0, 1, 3, 4 registers, and bit13, 12, 8 of BMCR(offset 62h-63h), the RTL8139D(L) must be placed in this mode.
+       This will prevent RTL8139D(L)'s configurations from accidental change
+*/
+enum Cfg9346Bits {
+	Cfg9346_Lock	= 0x00,
+	Cfg9346_Unlock	= 0xC0,
+};
+
+typedef enum {
+	CH_8139	= 0,
+	CH_8139_K,
+	CH_8139A,
+	CH_8139A_G,
+	CH_8139B,
+	CH_8130,
+	CH_8139C,
+	CH_8100,
+	CH_8100B_8139D,
+	CH_8101,
+} chip_t
+/*
+  Media Status Register : 
+			HasHltClk: BIT 0 R RXPF , Receive Pause Flag: Set, when RTL8139D(L) is in backoff state because a pause packet was received. Reset, when pause state is clear.
+			HasLWake : Bit 1 R TXPF , Transmit Pause Flag: Set, when RTL8139D(L) sends pause packet. Reset, when RTL8139D(L) sends a timer done packet.
+*/
+enum chip_flags {
+	HasHltClk	= (1 << 0),
+	HasLWake	= (1 << 1),
+};
+#define HW_REVID(b30, b29, b28, b27, b26, b23, b22)  (b30<<30 | b29<<29 | b28<<28 | b27<<27 | b26<<26 | b23<<23 | b22<<22)
+#define HW_REVID_MASK	HW_REVID(1, 1, 1, 1, 1, 1, 1)
+
+/* directly indexed by chip_t, above */
+static const struct {
+	const char *name;
+	u32 version; /* from RTL8139C/RTL8139D docs */
+	u32 flags;
+} rtl_chip_info[] = {
+	{ "RTL-8139",
+	  HW_REVID(1, 0, 0, 0, 0, 0, 0),
+	  HasHltClk,
+	},
+
+	{ "RTL-8139 rev K",
+	  HW_REVID(1, 1, 0, 0, 0, 0, 0),
+	  HasHltClk,
+	},
+
+	{ "RTL-8139A",
+	  HW_REVID(1, 1, 1, 0, 0, 0, 0),
+	  HasHltClk, /* XXX undocumented? */
+	},
+
+	{ "RTL-8139A rev G",
+	  HW_REVID(1, 1, 1, 0, 0, 1, 0),
+	  HasHltClk, /* XXX undocumented? */
+	},
+
+	{ "RTL-8139B",
+	  HW_REVID(1, 1, 1, 1, 0, 0, 0),
+	  HasLWake,
+	},
+
+	{ "RTL-8130",
+	  HW_REVID(1, 1, 1, 1, 1, 0, 0),
+	  HasLWake,
+	},
+
+	{ "RTL-8139C",
+	  HW_REVID(1, 1, 1, 0, 1, 0, 0),
+	  HasLWake,
+	},
+
+	{ "RTL-8100",
+	  HW_REVID(1, 1, 1, 1, 0, 1, 0),
+ 	  HasLWake,
+ 	},
+
+	{ "RTL-8100B/8139D",
+	  HW_REVID(1, 1, 1, 0, 1, 0, 1),
+	  HasHltClk /* XXX undocumented? */
+	| HasLWake,
+	},
+
+	{ "RTL-8101",
+	  HW_REVID(1, 1, 1, 0, 1, 1, 1),
+	  HasLWake,
+	},
+};
+
+struct rtl_extra_stats {
+	unsigned long early_rx;
+	unsigned long tx_buf_mapped;
+	unsigned long tx_timeouts;
+	unsigned long rx_lost_in_ring;
+};
+struct rtl8139_stats {
+	u64	packets;
+	u64	bytes;
+	struct u64_stats_sync	syncp;
+};
+
+struct rtl8139_private {
+	void __iomem		*mmio_addr;  /*memory mapped I/O addr */
+	int			drv_flags;    
+	struct pci_dev		*pci_dev;    /*PCI device */
+	u32			msg_enable; 
+	struct napi_struct	napi; /* Structure for NAPI scheduling similar to tasklet but with weighting */ 
+	struct net_device	*dev; /*  The NET DEVICE structure.*/
+
+	unsigned char		*rx_ring;
+	unsigned int		cur_rx;	  /* RX buf index of next pkt */
+	struct rtl8139_stats	rx_stats;
+	dma_addr_t		rx_ring_dma;
+
+	unsigned int		tx_flag;  /*tx_flag shall contain transmission flags to notify the device*/
+	unsigned long		cur_tx;   /*cur_tx shall hold current transmission descriptor*/
+	unsigned long		dirty_tx; /*dirty_tx denotes the first of transmission descriptors which have not completed transmission.*/
+	struct rtl8139_stats	tx_stats;  
+	unsigned char		*tx_buf[NUM_TX_DESC];	/* Tx bounce buffers */
+	unsigned char		*tx_bufs;	/* Tx bounce buffer region. */
+	dma_addr_t		tx_bufs_dma;
+
+	signed char		phys[4];	/* MII device addresses. */
+
+				/* Twister tune state. */
+	char			twistie, twist_row, twist_col;
+
+	unsigned int		watchdog_fired : 1;
+	unsigned int		default_port : 4; /* Last dev->if_port value. */
+	unsigned int		have_thread : 1;
+
+	spinlock_t		lock;
+	spinlock_t		rx_lock;
+
+	chip_t			chipset;
+	u32			rx_config;
+	struct rtl_extra_stats	xstats;
+
+	struct delayed_work	thread;  /*types of work structure */
+ 
+	struct mii_if_info	mii;   /*Media Independent Interface Support : Ethtool Support*/
+	unsigned int		regs_len;
+	unsigned long		fifo_copy_timeout;
+};
+module_param(use_io, bool, 0);
+MODULE_PARM_DESC(use_io, "Force use of I/O access mode. 0=MMIO 1=PIO");
+module_param(multicast_filter_limit, int, 0);
+module_param_array(media, int, NULL, 0);
+module_param_array(full_duplex, int, NULL, 0);
+module_param(debug, int, 0);
+MODULE_PARM_DESC (debug, "8139too bitmapped message enable number");
+MODULE_PARM_DESC (multicast_filter_limit, "8139too maximum number of filtered multicast addresses");
+MODULE_PARM_DESC (media, "8139too: Bits 4+9: force full duplex, bit 5: 100Mbps");
+MODULE_PARM_DESC (full_duplex, "8139too: Force full duplex for board(s) (1)");
+
+/* write MMIO register, with flush */
+/* Flush avoids rtl8139 bug w/ posted MMIO writes */
+#define RTL_W8_F(reg, val8)	do { iowrite8 ((val8), ioaddr + (reg)); ioread8 (ioaddr + (reg)); } while (0)
+#define RTL_W16_F(reg, val16)	do { iowrite16 ((val16), ioaddr + (reg)); ioread16 (ioaddr + (reg)); } while (0)
+#define RTL_W32_F(reg, val32)	do { iowrite32 ((val32), ioaddr + (reg)); ioread32 (ioaddr + (reg)); } while (0)
+
+/* write MMIO register */
+#define RTL_W8(reg, val8)	iowrite8 ((val8), ioaddr + (reg))
+#define RTL_W16(reg, val16)	iowrite16 ((val16), ioaddr + (reg))
+#define RTL_W32(reg, val32)	iowrite32 ((val32), ioaddr + (reg))
+
+/* read MMIO register */
+#define RTL_R8(reg)		ioread8 (ioaddr + (reg))
+#define RTL_R16(reg)		ioread16 (ioaddr + (reg))
+#define RTL_R32(reg)		ioread32 (ioaddr + (reg))
+
+static const u16 rtl8139_intr_mask =
+	PCIErr | PCSTimeout | RxUnderrun | RxOverflow | RxFIFOOver |
+	TxErr | TxOK | RxErr | RxOK;
+
+static const u16 rtl8139_norx_intr_mask =
+	PCIErr | PCSTimeout | RxUnderrun |
+	TxErr | TxOK | RxErr ;
+
+/* RxCfgRcv8K : 8K/16K/32K/64K | NO wrap | RX_FIFO_THRESH(7) << 14 i.e no rx threshold| RX_DMA_BURST(7) << 8 i.e unlimited DMA data bursts */
+#if RX_BUF_IDX == 0
+static const unsigned int rtl8139_rx_config =
+	RxCfgRcv8K | RxNoWrap |
+	(RX_FIFO_THRESH << RxCfgFIFOShift) |
+	(RX_DMA_BURST << RxCfgDMAShift);
+#elif RX_BUF_IDX == 1
+static const unsigned int rtl8139_rx_config =
+	RxCfgRcv16K | RxNoWrap |
+	(RX_FIFO_THRESH << RxCfgFIFOShift) |
+	(RX_DMA_BURST << RxCfgDMAShift);
+#elif RX_BUF_IDX == 2
+static const unsigned int rtl8139_rx_config =
+	RxCfgRcv32K | RxNoWrap |
+	(RX_FIFO_THRESH << RxCfgFIFOShift) |
+	(RX_DMA_BURST << RxCfgDMAShift);
+#elif RX_BUF_IDX == 3
+static const unsigned int rtl8139_rx_config =
+	RxCfgRcv64K |
+	(RX_FIFO_THRESH << RxCfgFIFOShift) |
+	(RX_DMA_BURST << RxCfgDMAShift);
+#else
+#error "Invalid configuration for 8139_RXBUF_IDX"
+#endif
+/* rtl8139_tx_config : TxIFG96 () | (TX_DMA_BURST (6)<< TxDMAShift(8)) i.e 1024 | TX_RETRY(8 = 1000) << TxRetryShift (4) : retry =16*/
+static const unsigned int rtl8139_tx_config =
+	TxIFG96 | (TX_DMA_BURST << TxDMAShift) | (TX_RETRY << TxRetryShift);
+
+/* New device inserted : probe function -*/
+static int  rtl8139_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
-	pr_info("%s: probe invoked\n",__func__);
+	struct net_device *dev = NULL;
+	struct rtl8139_private *tp;                /*our private data structures,*/
+	int i, addr_len, option; 
+	void __iomem *ioaddr;                      /*IO MEM Addresse*/
+	static int board_idx = -1;
+	assert (pdev != NULL);
+	assert (ent != NULL );
+	board_idx++;
+	
+	/* when we're built into the kernel, the driver version message
+	 * is only printed if at least one 8139 board has been found
+	 */
+#ifndef MODULE
+	{
+		static int printed_version;
+		if (!printed_version++)
+			pr_info(RTL8139_DRIVER_NAME "\n");
+	}
+#endif
+	/*pdev->revision:  0x20 for enhanced 8139C+ version*/ 
+	if(pdev->vendor == PCI_VENDOR_ID_REALTEK && 
+		pdev->device == PCI_DEVICE_ID_REALTEK_8139 && pdev->revision >= 0x20){
+		dev_info(&pdev->dev,
+			   "This (id %04x:%04x rev %02x) is an enhanced 8139C+ chip, use 8139cp\n",
+		       	   pdev->vendor, pdev->device, pdev->revision);
+		return -ENODEV;
+	}
+		
 	return 0;
 }
 static void rtl8139_remove_one(struct pci_dev *dev)
